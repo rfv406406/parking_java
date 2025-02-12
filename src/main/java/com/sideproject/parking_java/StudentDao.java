137b2abe4ddeacf460b300a5b32b812ad44f2015
Zhui Kang <rfv406406@gmail.com>
2025-02-12 18:55:39 +0800

    add utility package

--- src/main/java/com/sideproject/parking_java/StudentDao.java
+++ src/main/java/com/sideproject/parking_java/StudentDao.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import com.sideproject.parking_java.Model.TimeModel;
+import com.sideproject.parking_java.Model.TimeFormatModel;
 
 @Component
 public class StudentDao {
@@ -16,12 +16,15 @@ public class StudentDao {
 
     public String insertStudent(Student student) {
         String sql = "INSERT INTO student(number, name, email, creatTime) VALUES (:number, :name, :email, :creatTime)";
-        // LocalDateTime creatTime = LocalDateTime.now().withNano(0);        
+        // LocalDateTime creatTime = LocalDateTime.now().withNano(0);       
         HashMap<String, Object> map = new HashMap<>();
         map.put("number", student.getNumber());
         map.put("name", student.getName());
         map.put("email", student.getEmail());
-        map.put("creatTime", TimeModel.timeFormat(new Date()));
+        map.put("creatTime", TimeFormatModel.timeFormat(new Date()));
+         if (true) {
+            throw new RuntimeException("123");
+        } 
         namedParameterJdbcTemplate.update(sql, map);
         return "INSERT";
     }
