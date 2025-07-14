103531668380e97a32b84c05ecc1c9e74b1ccaf7
rfv406406 <rfv406406@gmail.com>
2025-07-14 09:37:50 +0800

    elasticache testing 18

--- src/main/java/com/sideproject/parking_java/redis/RedisKeyExpirationService.java
+++ src/main/java/com/sideproject/parking_java/redis/RedisKeyExpirationService.java
@@ -1,30 +1,30 @@
-package com.sideproject.parking_java.redis;
+// package com.sideproject.parking_java.redis;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.stereotype.Service;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.data.redis.connection.Message;
+// import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+// import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class RedisKeyExpirationService extends KeyExpirationEventMessageListener {
+// @Service
+// public class RedisKeyExpirationService extends KeyExpirationEventMessageListener {
 
-    final static Logger logger = LoggerFactory.getLogger(RedisKeyExpirationService.class);
+//     final static Logger logger = LoggerFactory.getLogger(RedisKeyExpirationService.class);
 
-    public RedisKeyExpirationService(RedisMessageListenerContainer listenerContainer) {
-        super(listenerContainer);
-    }
+//     public RedisKeyExpirationService(RedisMessageListenerContainer listenerContainer) {
+//         super(listenerContainer);
+//     }
 
-    @Override
-    public void doHandleMessage(@SuppressWarnings("null") Message message) {
+//     @Override
+//     public void doHandleMessage(@SuppressWarnings("null") Message message) {
         
-        byte[] body = message.getBody();
+//         byte[] body = message.getBody();
         
-        byte[] channel = message.getChannel();
+//         byte[] channel = message.getChannel();
         
-        logger.info("message = {}, channel = {}", new String(body), new String(channel));
-        System.out.println("body: " + new String(body));
-        System.out.println("channel: " + new String(channel));
-    }
-}
+//         logger.info("message = {}, channel = {}", new String(body), new String(channel));
+//         System.out.println("body: " + new String(body));
+//         System.out.println("channel: " + new String(channel));
+//     }
+// }
