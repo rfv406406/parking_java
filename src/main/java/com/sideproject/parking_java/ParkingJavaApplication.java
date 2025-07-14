5d56257eee85ef2ff7f46c77f8a0373296f9d259
rfv406406 <rfv406406@gmail.com>
2025-07-14 10:29:38 +0800

    elasticache testing 19

--- src/main/java/com/sideproject/parking_java/ParkingJavaApplication.java
+++ src/main/java/com/sideproject/parking_java/ParkingJavaApplication.java
@@ -2,6 +2,7 @@ package com.sideproject.parking_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ParkingJavaApplication {
 
